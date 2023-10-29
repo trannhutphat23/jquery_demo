e91fa120ad8737f61f1f7662e56c02fb2405c917
trannhutphat23 <trannhutphattv@gmail.com>
2023-10-29 15:29:25 +0700

    new project

---
+++ index.js
@@ -0,0 +1,148 @@
+function render(info){
+    const userInfoHtml = info.map(item => {
+        return `
+            <div class="user-info-card" id=`+item.id+`>
+                <div class="header-info">
+                    <img class="user-avatar" src="`+ item.image +`" alt="" />
+                    <div class="user-detail">
+                        <h3 class="user-name">` + item.name +`</h3>
+                        <p class="user-description">`+ item.description +`</p>
+                        <p class="user-id">`+ item.id +`</p>
+                    </div>
+                </div>
+                <div class="horizontalLine"></div>
+                <div class="detail_info">
+                    <div class="user-age">
+                        <div class="age_label">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M86.4 5.5L61.8 47.6C58 54.1 56 61.6 56 69.2V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L105.6 5.5C103.6 2.1 100 0 96 0s-7.6 2.1-9.6 5.5zm128 0L189.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L233.6 5.5C231.6 2.1 228 0 224 0s-7.6 2.1-9.6 5.5zM317.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L361.6 5.5C359.6 2.1 356 0 352 0s-7.6 2.1-9.6 5.5L317.8 47.6zM128 176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48c-35.3 0-64 28.7-64 64v71c8.3 5.2 18.1 9 28.8 9c13.5 0 27.2-6.1 38.4-13.4c5.4-3.5 9.9-7.1 13-9.7c1.5-1.3 2.7-2.4 3.5-3.1c.4-.4 .7-.6 .8-.8l.1-.1 0 0 0 0s0 0 0 0s0 0 0 0c3.1-3.2 7.4-4.9 11.9-4.8s8.6 2.1 11.6 5.4l0 0 0 0 .1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c3-3.5 7.4-5.4 12-5.4s9 2 12 5.4l.1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c2.9-3.4 7.1-5.3 11.6-5.4s8.7 1.6 11.9 4.8l0 0 0 0 0 0 .1 .1c.2 .2 .4 .4 .8 .8c.8 .7 1.9 1.8 3.5 3.1c3.1 2.6 7.5 6.2 13 9.7c11.2 7.3 24.9 13.4 38.4 13.4c10.7 0 20.5-3.9 28.8-9V288c0-35.3-28.7-64-64-64V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H256V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H128V176zM448 394.6c-8.5 3.3-18.2 5.4-28.8 5.4c-22.5 0-42.4-9.9-55.8-18.6c-4.1-2.7-7.8-5.4-10.9-7.8c-2.8 2.4-6.1 5-9.8 7.5C329.8 390 310.6 400 288 400s-41.8-10-54.6-18.9c-3.5-2.4-6.7-4.9-9.4-7.2c-2.7 2.3-5.9 4.7-9.4 7.2C201.8 390 182.6 400 160 400s-41.8-10-54.6-18.9c-3.7-2.6-7-5.2-9.8-7.5c-3.1 2.4-6.8 5.1-10.9 7.8C71.2 390.1 51.3 400 28.8 400c-10.6 0-20.3-2.2-28.8-5.4V480c0 17.7 14.3 32 32 32H416c17.7 0 32-14.3 32-32V394.6z"/></svg>
+                            <span>Age</span>
+                        </div>
+                        <div>`+ item.age +`</div>
+                    </div>
+                    <div class="user-email">
+                        <div class="email_label">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg>
+                            <span>Email</span>
+                        </div>
+                        <div>`+ item.email +`</div>
+                    </div>
+                    <div class="user-phone">
+                        <div class="phone_label">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+                            <span>Phone</span>
+                        </div>
+                        <div>`+ item.phone +`</div>
+                    </div>
+                </div>
+                <button type="button" class="btn_detail" id="btn-`+item.id+`">PROFILE</button>
+            </div> `
+    })
+    $(".info").html(userInfoHtml.join(''));
+}
+
+$(document).ready(function(){
+    // Set default for search input
+    $(".search-container").addClass("hidden");
+    // Click tab thông tin chung
+    $("#generalInfo").click(function(){
+        $(".InfoGroup").show();
+        $(".info").hide();
+        $(".search-container").addClass("hidden");
+    })
+    // Click tab thành viến
+    $("#infoTeam").click(function(){
+        $(".InfoGroup").hide();
+        $(".info").show();
+        $(".search-container").removeClass("hidden");
+        $.ajax({
+            type: 'GET',
+            url: './data.json',
+            dataType: 'json',
+            success: function(info){
+                render(info);
+            }
+        })
+    })
+    // Xử lý click event search button
+    $(".search-button").click(function(){
+        let valueInput = $(".search-input").val();
+        $(".search-input").val("");
+        if (valueInput == ""){
+            $.ajax({
+                type: 'GET',
+                url: './data.json',
+                dataType: 'json',
+                success: function(info){
+                    render(info);
+                }
+            })
+        }
+        else{
+            $.ajax({
+                type: 'GET',
+                url: './data.json',
+                dataType: 'json',
+                success: function(info){
+                    const userInfoHtml = info.map(item => {
+                        if (item.name == valueInput){
+                            return `
+                                <div class="user-info-card" id=`+item.id+`>
+                                    <div class="header-info">
+                                        <img class="user-avatar" src="`+ item.image +`" alt="" />
+                                        <div class="user-detail">
+                                            <h3 class="user-name">` + item.name +`</h3>
+                                            <p class="user-description">`+ item.description +`</p>
+                                            <p class="user-id">`+ item.id +`</p>
+                                        </div>
+                                    </div>
+                                    <div class="horizontalLine"></div>
+                                    <div class="detail_info">
+                                        <div class="user-age">
+                                            <div class="age_label">
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M86.4 5.5L61.8 47.6C58 54.1 56 61.6 56 69.2V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L105.6 5.5C103.6 2.1 100 0 96 0s-7.6 2.1-9.6 5.5zm128 0L189.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L233.6 5.5C231.6 2.1 228 0 224 0s-7.6 2.1-9.6 5.5zM317.8 47.6c-3.8 6.5-5.8 14-5.8 21.6V72c0 22.1 17.9 40 40 40s40-17.9 40-40V69.2c0-7.6-2-15-5.8-21.6L361.6 5.5C359.6 2.1 356 0 352 0s-7.6 2.1-9.6 5.5L317.8 47.6zM128 176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48c-35.3 0-64 28.7-64 64v71c8.3 5.2 18.1 9 28.8 9c13.5 0 27.2-6.1 38.4-13.4c5.4-3.5 9.9-7.1 13-9.7c1.5-1.3 2.7-2.4 3.5-3.1c.4-.4 .7-.6 .8-.8l.1-.1 0 0 0 0s0 0 0 0s0 0 0 0c3.1-3.2 7.4-4.9 11.9-4.8s8.6 2.1 11.6 5.4l0 0 0 0 .1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c3-3.5 7.4-5.4 12-5.4s9 2 12 5.4l.1 .1c.1 .1 .4 .4 .7 .7c.7 .7 1.7 1.7 3.1 3c2.8 2.6 6.8 6.1 11.8 9.5c10.2 7.1 23 13.1 36.3 13.1s26.1-6 36.3-13.1c5-3.5 9-6.9 11.8-9.5c1.4-1.3 2.4-2.3 3.1-3c.3-.3 .6-.6 .7-.7l.1-.1c2.9-3.4 7.1-5.3 11.6-5.4s8.7 1.6 11.9 4.8l0 0 0 0 0 0 .1 .1c.2 .2 .4 .4 .8 .8c.8 .7 1.9 1.8 3.5 3.1c3.1 2.6 7.5 6.2 13 9.7c11.2 7.3 24.9 13.4 38.4 13.4c10.7 0 20.5-3.9 28.8-9V288c0-35.3-28.7-64-64-64V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H256V176c0-17.7-14.3-32-32-32s-32 14.3-32 32v48H128V176zM448 394.6c-8.5 3.3-18.2 5.4-28.8 5.4c-22.5 0-42.4-9.9-55.8-18.6c-4.1-2.7-7.8-5.4-10.9-7.8c-2.8 2.4-6.1 5-9.8 7.5C329.8 390 310.6 400 288 400s-41.8-10-54.6-18.9c-3.5-2.4-6.7-4.9-9.4-7.2c-2.7 2.3-5.9 4.7-9.4 7.2C201.8 390 182.6 400 160 400s-41.8-10-54.6-18.9c-3.7-2.6-7-5.2-9.8-7.5c-3.1 2.4-6.8 5.1-10.9 7.8C71.2 390.1 51.3 400 28.8 400c-10.6 0-20.3-2.2-28.8-5.4V480c0 17.7 14.3 32 32 32H416c17.7 0 32-14.3 32-32V394.6z"/></svg>
+                                                <span>Age</span>
+                                            </div>
+                                            <div>`+ item.age +`</div>
+                                        </div>
+                                        <div class="user-email">
+                                            <div class="email_label">
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg>
+                                                <span>Email</span>
+                                            </div>
+                                            <div>`+ item.email +`</div>
+                                        </div>
+                                        <div class="user-phone">
+                                            <div class="phone_label">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+                                                <span>Phone</span>
+                                            </div>
+                                            <div>`+ item.phone +`</div>
+                                        </div>
+                                    </div>
+                                    <button type="button" class="btn_detail" id="btn-`+item.id+`">PROFILE</button>
+                                </div> `
+                        }
+                    })
+                    $(".info").html(userInfoHtml.join(''));
+                }
+            })
+        }
+    
+    })  
+
+    // Effect hover
+    $(".info").on("mouseenter", ".user-info-card", function(){
+        var getID = $(this).children('button').attr('id');
+        $("#"+getID).show();
+        $(this).css("backgroundColor", "rgb(209, 207, 207)");
+        $(".btn_detail").on("click", function(){
+            var getID = $(this).parent().closest('.user-info-card').attr('id');
+            location.href = "./profile.html?id="+getID+"";
+        })
+    })
+    $(".info").on("mouseleave", ".user-info-card", function(){
+        var getID = $(this).children('button').attr('id');
+        $("#"+getID).hide();
+        $(this).css("backgroundColor", "white");
+    })
+})
---
+++ profile.js
@@ -0,0 +1,61 @@
+var url = "./data.json"
+var query = window.location.search.substring(1);
+var id = query.split('=')[1]
+$(document).ready(function(){
+
+    $.ajax({
+        type: 'GET',
+        url: './data.json',
+        dataType: 'json',
+        success: function(info){
+            const htmls = info.map(data => {
+                if (id == data.id){
+                    return `
+                        <div class="profile_detail">
+                            <a href="index.html"><button class="back-button">Back</button></a>
+                            <img src="`+ data.image +`" alt="`+ data.name +`">
+                            <h3>`+ data.name +`</h3>
+                            <h2>About Me</h2>
+                            <p>`+ data.AboutMe +`</p>
+                            
+                            <div class="info">
+                                <div class="InfoContact">
+                                    <h2>Contact Information</h2>
+                                    <hr>
+                                    <p>Email: <a href="mailto:`+data.email+`">`+data.email+`</a></p>
+                                    <p>Số điện thoại: <a href="#">`+data.phone+`</a></p>
+                                </div>
+                                <div class="InfoSocial">
+                                    <h2>Social Media</h2>
+                                    <hr>
+                                    <i class="fa-brands fa-facebook" style="color: #005eff; font-size: 40px; margin-right: 20px; cursor: pointer;"></i>
+                                    <i class="fa-brands fa-instagram instagram"></i>
+                                    <i class="fa-brands fa-twitter" style="color: #60b3e6; font-size: 40px; cursor: pointer; margin-right: 20px;"></i>
+                                </div>
+                            </div>
+                        </div>
+                    `
+                }
+            });
+            $("#profile").html(htmls.join(''));
+        }
+    })
+
+    $.ajax({
+        type: 'GET',
+        url: './data.json',
+        dataType: 'json',
+        success: function(info){
+            const htmls = info.map(data => {
+                if (id != data.id){
+                    return `
+                        <a href="profile.html?id=`+ data.id +`">
+                            <img src="`+ data.image +`" alt="`+ data.name +`">
+                        </a>
+                    `
+                }
+            });
+            $("#members").html(htmls.join(''));
+        }
+    })
+})
